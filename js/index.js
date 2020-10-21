dceac450ef73f8856ce3802cd49a4ef3c8b39e52
aparr075 <aparr075@fiu.edu>
2020-10-21 12:03:53 -0400

    first iteration

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
-
+console.log("I'm ready!")
+let hacker1 = `ana`;
+console.log("The driver's name is " + hacker1)
+let hacker2 = `daniel`;
+console.log("The navigator's name is " + hacker2)
 
 // Iteration 2: Conditionals
 
